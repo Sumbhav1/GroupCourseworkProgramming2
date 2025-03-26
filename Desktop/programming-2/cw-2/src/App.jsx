9c1ff8bf50141de62bc25113cde0871760c6910a
Sumbhav Mishra <sumbhavmishra@172-26-16-112.eduroam.bath.ac.uk>
2025-03-26 13:55:32 +0000

    initial commit - Vite + Flask setup

---
+++ Desktop/programming-2/cw-2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>
+        hello
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
